2a9b7012fe735d2d85d699eb8fb1a6dd65507d68
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-06-17 19:57:50 +0530

    chore: Refactor auth layout component

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,17 +1,9 @@
-import Image from "next/image";
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <main className="relative h-screen w-full">
-      <div className="absolute size-full">
-        <Image src="/images/bg-img.png" alt="background" fill className="size-full" />
+const Layout = ({children}: {children: React.ReactNode}) => {
+  return(
+      <div className="flex-center min-h-screen w-full bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center">
+        {children}  
       </div>
-
-      {children}
-    </main>
-  );
+  )
 }
+
+export default Layout
