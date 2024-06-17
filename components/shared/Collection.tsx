d53afa4be879436787c5068e3bd51a9d49ccae1a
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-06-17 23:41:28 +0530

    chore: Add pagination to Collection component

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -1,5 +1,6 @@
 import { IEvent } from "@/lib/database/models/event.model";
 import Card from "./Card";
+import Pagination from "./Pagination";
 
 type CollectionProps = {
   data: IEvent[];
@@ -40,6 +41,9 @@ const Collection = ({
            )
           })}
          </ul>
+         {totalPages > 1 && (
+            <Pagination urlParamName={urlParamName} page={page} totalPages={totalPages} />
+         )}
         </div>
       ) : (
         <div className="flex-center wrapper min-h-[200px]w-full flex-col gap-3 rounded-[14px] bg-grey-50 py-28 text-center">
